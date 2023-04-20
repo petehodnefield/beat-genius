2bfb738647dceb19d90c1667cb099521495069a1
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-04-20 11:56:28 -0500

    circle icon component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import djSet from "../../../public/assets/images/dj-set.jpg";
 import { Icon } from "@iconify/react";
+import CircleIcon from "../Icons/CircleIcon";
 
 const Hero = () => {
   return (
@@ -29,6 +30,7 @@ const Hero = () => {
           src="https://images.unsplash.com/photo-1594623930572-300a3011d9ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
         />
       </div>
+      <CircleIcon iconPosition={"hello"} iconCode="ph:microphone-stage" />
     </section>
   );
 };
---
+++ src/components/Icons/CircleIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+const CircleIcon = ({ iconCode, iconPosition }) => {
+  return (
+    <div className={`circle-icon ${iconPosition}`}>
+      <div className="circle-icon__outer-shadow">
+        <div className="circle-icon__inner-shadow">
+          <Icon className="circle-icon__icon" icon={iconCode} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CircleIcon;
