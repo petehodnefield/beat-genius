3cb1e1302b4f0a390a1b60f172924162194364ee
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-04-20 12:28:37 -0500

    growth pill component added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,12 @@
 import React from "react";
+import GrowthPill from "../Graphics/GrowthPill";
 
 const About = () => {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <GrowthPill />
+    </div>
+  );
 };
 
 export default About;
---
+++ src/components/Graphics/GrowthPill.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const GrowthPill = () => {
+  return (
+    <div className="growth-pill">
+      <div className="growth-pill__chart">
+        <span className="growth-pill__chart-text">+10k</span>
+      </div>
+      <p className="growth-pill__text">
+        New followers <br /> this month
+      </p>
+    </div>
+  );
+};
+
+export default GrowthPill;
