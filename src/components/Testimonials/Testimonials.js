745c72b37a76c67b32659d76c5b0a14c6821a17b
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-04-25 10:10:39 -0500

    Testimonial carousel overflow x issue

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -4,6 +4,8 @@ import TestimonialsCarousel from "./TestimonialsCarousel";
 const Testimonials = () => {
   return (
     <section className="testimonials">
+      {/* Gradient background */}
+      <div className="testimonials__gradient"></div>
       <div className="testimonials-heading-wrapper">
         <h2 className="testimonials__header">What people say</h2>
         <h3 className="testimonials__subheader">
