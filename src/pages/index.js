5e3057b1a68867c95ad4e13a322acf04d85fa5f8
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-04-29 08:49:43 -0500

    Cta optimized for tablet and mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
         <Pricing />
         <Testimonials />
         {/* <SuccessStories /> */}
-        {/* <CTASection /> */}
+        <CTASection />
       </main>
       <Footer />
     </div>
