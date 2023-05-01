13960ee6728c41d3357ac991c0e8a895f450b9c1
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-05-01 08:44:33 -0500

    removed old stylesheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Plus_Jakarta_Sans } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
